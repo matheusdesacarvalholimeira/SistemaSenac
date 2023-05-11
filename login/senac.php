e7f4bc2d8f173934e049ae63992108ad59c88e61
Ítalo Nunes <37880520+italo2403@users.noreply.github.com>
2023-05-11 19:12:00 -0300

    Sistema Senac Provas
    Criação do Sistema Senac para provas internas dos Alunos.

---
+++ login/senac.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <style>
+        body{ font: 14px sans-serif; }
+        .wrapper{ 
+            width: 360px; 
+            padding: 20px;
+            margin-left: 30%;
+         }
+    </style>
+    <title>Prova Senac</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm " id="bgnav">
+    <!-- logo -->
+    <a class="navbar-brand" href="#">
+      <img src="img/logo-senac.png" alt="logo" style="width:65px;" class="logoo">
+    </a>
+    <!-- Links -->
+    
+  </nav>
+  
+<div class="container">
+    <div class="row">
+        <div class="col">
+        <img src="./img/questao1.png" class="img-thumbnail" alt="...">
+        <label for="">Insira sua Resposta</label>
+        <input type="text" name="" id="">
+        
+         
+        </div>
+    </div>
+</div>
+</body>
+</html>
